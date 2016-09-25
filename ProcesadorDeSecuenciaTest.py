549fa8a2258fe2ab8fd349840dbabc81d80afd23
davidaso28 <da.saavedra@uniandes.edu.co>
2016-09-25 18:32:26 -0500

    Paso2-Se agrega prueba para probar con un numero(Rojo)

--- ProcesadorDeSecuenciaTest.py
+++ ProcesadorDeSecuenciaTest.py
@@ -4,3 +4,6 @@ from ProcesadorDeSecuencia import ProcesadorDeSecuencia
 class ProcesadorDeSecuenciaTest(TestCase):
     def test_cantidadDeElementos(self):
         self.assertEqual(ProcesadorDeSecuencia().cantidadDeElementos(""),0,"Cadena vacia")
+
+    def test_cantidadDeElementosUnNumero(self):
+        self.assertEqual(ProcesadorDeSecuencia().cantidadDeElementos("1"),1,"Cadena Un numero")
