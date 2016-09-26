6340a5e5fee6612f0c124abc7391d93e363cb7db
davidaso28 <da.saavedra@uniandes.edu.co>
2016-09-25 19:50:19 -0500

    Iteración Numero 3-Paso3 cantidad,minimo,maximo dos  numeros (verde)

--- ProcesadorDeSecuencia.py
+++ ProcesadorDeSecuencia.py
@@ -16,6 +16,12 @@ class ProcesadorDeSecuencia:
     def cantidadDeElementosYMaximo(self,cadena):
         if cadena == "":
             return [0,None,None]
+        elif "," in cadena:
+            if(int(cadena[0])<int(cadena[2])):
+                return [2,int(cadena[0]),int(cadena[2])]
+            else:
+                return [2, int(cadena[2]), int(cadena[0])]
+
         else:
             return [1,int(cadena),int(cadena)]
 
